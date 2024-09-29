a4e62030faeb8698b864fd4da563394186203753
Irving Lucas <irving.lucas@gmail.com>
2024-09-29 17:59:40 -0300

    Section 2 - Configure and Register tasks
    Adding code related to lesson 1, regarding the configuration of tasks
    that are added by plugins.

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -6,3 +6,49 @@ plugins {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8" // best practice
+}
+
+// another way to configure tasks if the accessor is not available for some reason
+//tasks.named("clean") {
+//    println("THE TASK $name FROM ${project.name} WAS SUCCESSFULLY RAN.")
+//}
+
+allprojects.forEach { project ->
+    if (project.name == "app") {
+        tasks.named("clean") {
+            println("The task named CLEAN, from APP project, has its own string.")
+        }
+    }
+
+    if (project.name == "business-logic") {
+        tasks.named("clean") {
+            println("This task cleans everything about business-logic project :)")
+        }
+    }
+
+    if (project.name == "data-model") {
+        tasks.named("clean") {
+            println("This task makes this project clean when it's about data-model")
+        }
+    }
+}
+
+//
+//tasks.compileJava {
+//    options.encoding = "UTF-8"
+//}
+//
+//tasks.compileTestJava {
+//    options.encoding = "UTF-8"
+//}
+
+//tasks.test {
+//
+//}
+//
+//tasks.javadoc {
+//
+//}
